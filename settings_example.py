daa0bffb1a7a2d23f7f528c269d56d1b95c246df
Ivan Sudarev <ivan.sudarev@gmail.com>
2024-09-21 20:41:56 +0300

    Init

---
+++ settings_example.py
@@ -0,0 +1,7 @@
+# To use this script you'll need to register an app at https://my.telegram.org/apps
+# in order to get API_ID and API_HASH
+API_ID = 'your_api_id' 
+API_HASH = 'your_api_hasg'
+
+CHANNEL_ADDRESS = 'channel_name_without_@'
+POSTS_LIMIT = 1500
